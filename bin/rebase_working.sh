eaf6c9a28266acd5c682686179bc3979d7a03b03
Sean Lee <seanlee@Seans-MacBook-Pro.local>
2017-04-18 10:43:01 +0800

    Implement shell stack structure and improve rebase_working.sh.

--- bin/rebase_working.sh
+++ bin/rebase_working.sh
@@ -29,18 +29,16 @@ git rebase central/default $MASTER
 git push $TARGET_REMOTE HEAD
 echo ""
 
-for i in `cat ${WORKING_LIST_FILE}`;do
-  branchName=`echo $i | sed "s/^[-]*//"`
-  echo "====> Working on branch: ["${CL_GREEN}${branchName}${CL_END}"] <===="
-  if [[ $i == -* ]]; then
-    _currentParent=$parent
-  else
+while IFS= read -r line; do
+  branchName=`echo $line | awk '{print $1}'`
+  _currentParent=`echo $line | awk '{print $2}'`
+  if [ -z "${_currentParent}" ]; then
     _currentParent=$MASTER
-    parent=$branchName
   fi
+  echo "====> Working on branch: ["${CL_GREEN}${branchName}${CL_END}"] <===="
   echo "                Rebasing ["${branchName}"]"
   echo "                based on ["${CL_BLUE}${_currentParent}${CL_END}"] ..."
   git rebase $_currentParent $branchName
   git push $TARGET_REMOTE HEAD -f
-  echo ""
-done
+done <"$WORKING_LIST_FILE"
+
