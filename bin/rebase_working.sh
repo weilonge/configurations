15c68cd81b73dcd9d0a04bf682e5a77a21d39697
Sean Lee <seanlee@Seans-MacBook-Pro.local>
2017-05-17 09:47:17 +0800

    rebase_working.sh supports comments and configs now.

--- bin/rebase_working.sh
+++ bin/rebase_working.sh
@@ -24,12 +24,35 @@ CL_END=`tput op`
 # echo "Let's fetch all changes..."
 # git remote update
 
+# grep "^@" $WORKING_LIST_FILE | while read -r line; do
+#   CCC=`echo $line | sed 's/^@//'`
+#   echo "[Config] $CCC"
+#   eval $CCC
+# done
+
+# echo $MASTER
+# echo $TARGET_REMOTE
+
+# exit
+
 echo "====> Synchronizing m-c from central to ${TARGET_REMOTE} <===="
 git rebase central/default $MASTER
 git push $TARGET_REMOTE HEAD
 echo ""
 
 while IFS= read -r line; do
+  set +e
+  COMMENT=`echo $line | grep "^#"`
+  if [ -n "${COMMENT}" ]; then
+    echo "$line"
+    continue
+  fi
+  CONFIG=`echo $line | grep "^@"`
+  if [ -n "${CONFIG}" ]; then
+    echo "[Config] "`echo $line | sed 's/^@//'`
+    continue
+  fi
+  set -e
   branchName=`echo $line | awk '{print $1}'`
   _currentParent=`echo $line | awk '{print $2}'`
   if [ -z "${_currentParent}" ]; then
