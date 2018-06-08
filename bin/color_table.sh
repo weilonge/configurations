ceb4ad02a2c684882799fa7b5d3259868183dfe3
Sean Lee <weilonge@gmail.com>
2018-06-09 03:23:00 +0800

    Improve the utils of color palette

--- bin/color_table.sh
+++ bin/color_table.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 printf '==== [ COLOR TABLE ] ====\n'
 for i in {0..255} ; do
-  printf "\x1b[38;5;%03dmcolour%-3d  " "${i}" "${i}"
+  printf "\e[38;5;%03dmcolour%-3d  " "${i}" "${i}"
   if [ "$(( i % 8 ))" -eq "7" ];then
     printf "\n"
   fi
@@ -10,5 +10,5 @@ done
 printf '\n'
 
 # Reset the terminal color to original one.
-echo -ne "\033[0m"
+printf "\e[0m\n"
 
